5bc96a87b6df3694fe321053b6453bd9e7d89b90
Irfan140 <irfanmehmud140@gmail.com>
2025-08-01 08:26:07 +0530

    Make the UI of Home Screen

---
+++ components/SearchBar.tsx
@@ -0,0 +1,30 @@
+import { icons } from "@/constants/icons";
+import { Image, TextInput, View } from "react-native";
+
+interface Props {
+  placeholder: string;
+  onPress?: () => void; // goal of onPress is not to return someting but rather to push through a different url
+}
+
+const SearchBar = ({ placeholder, onPress }: Props) => {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="w-5 h-5"
+        resizeMode="contain"
+        tintColor="#AB8BFF"
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value=""
+        onChangeText={() => {}}
+        className="flex-1 ml-2 text-white"
+        placeholderTextColor="#A8B5DB"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
